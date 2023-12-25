83da7dd3e81de7f3f22f678c5dbff54a9fce4c1d
Charly Huang <charly.charlongo@gmail.com>
2023-12-24 16:31:50 -0800

    Make Status class printable

--- include/status.h
+++ include/status.h
@@ -1,6 +1,8 @@
 #ifndef _HOMEBREW_BEHAVIOR_TREE_STATUS_H_
 #define _HOMEBREW_BEHAVIOR_TREE_STATUS_H_
 
+#include <iostream>
+
 namespace Homebrew_Behavior_Tree {
 
 enum class Status {
@@ -9,6 +11,27 @@ enum class Status {
     FAILURE,
     RUNNING
 };
+
+std::ostream& operator << (std::ostream& os, const Status& status) {
+    switch (status) {
+        case Status::IDLE:
+            os << "IDLE";
+            break;
+        case Status::SUCCESS:
+            os << "SUCCESS";
+            break;
+        case Status::FAILURE:
+            os << "FAILURE";
+            break;
+        case Status::RUNNING:
+            os << "RUNNING";
+            break;
+        default:
+            os << "UNKNOWN";
+            break;
+    }
+    return os;
+}
 
 } /* namespace */
 
