29b59a9df24f50e3330d6db8dbc2d997c598a547
Charly Huang <charly.charlongo@gmail.com>
2023-12-26 16:47:43 -0800

    Implement the logic for the tick() of the Sequence node

--- src/sequence_node.cpp
+++ src/sequence_node.cpp
@@ -8,17 +8,25 @@ SequenceNode::SequenceNode(const std::string& nodeName, const Status& status)
 
 Status SequenceNode::tick() {
     if (children_.empty()) {
-        return Status::FAILURE;
+        throw std::runtime_error("SequenceNode::tick() - No children to tick");
     }
-    // TODO
 
-    // DEBUG
-    return Status::SUCCESS;
-}
+    for (auto& child : children_) {
+        status_ = child->tick();
+        if (status_ == Status::FAILURE or status_ == Status::RUNNING) {
+            return status_;
+        }
+        else {
+            continue;
+        }
+    } /* for */
+    // Expect to return SUCCESS status
+    return status_;
+} /* tick */
 
 SequenceNode::Ptr
 SequenceNode::create(const std::string& nodeName, const Status& status) {
     return std::make_shared<SequenceNode>(nodeName, status);
-}
+} /* create */
 
 } /* namespace */
