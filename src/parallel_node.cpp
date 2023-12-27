21d4fa97bca3590ec3ecdaf2d1a9c5a8d78c8459
Charly Huang <charly.charlongo@gmail.com>
2023-12-26 16:46:46 -0800

    Implement the logic for Parallel node logic

--- src/parallel_node.cpp
+++ src/parallel_node.cpp
@@ -8,12 +8,31 @@ ParallelNode::ParallelNode(const std::string& nodeName, const Status& status)
 
 Status ParallelNode::tick() {
     if (children_.empty()) {
-        return Status::FAILURE;
+        throw std::runtime_error("ParallelNode::tick() - No children to tick");
     }
-    // TODO
 
-    // DEBUG
-    return Status::SUCCESS;
+    uint successCount{0};
+    uint failureCount{0};
+    for (auto& child : children_) {
+        Status childStatus = child->tick();
+        if (childStatus == Status::SUCCESS) {
+            ++successCount;
+        }
+        else if (childStatus == Status::FAILURE) {
+            ++failureCount;
+        }
+    } /* for */
+    if (successCount == children_.size()) {
+        status_ = Status::SUCCESS;
+    }
+    else if (failureCount > 0) {
+        status_ = Status::FAILURE;
+    }
+    else {
+        status_ = Status::RUNNING;
+    }
+
+    return status_;
 }
 
 ParallelNode::Ptr
