c406d6af4a636d5dfe98fab063b2984d5e70c521
Charly Huang <charly.charlongo@gmail.com>
2023-12-26 16:45:05 -0800

    Implement the logic for the tick() of Fallback node

--- src/fallback_node.cpp
+++ src/fallback_node.cpp
@@ -8,12 +8,29 @@ FallbackNode::FallbackNode(const std::string& nodeName, const Status& status)
 
 Status FallbackNode::tick() {
     if (children_.empty()) {
-        return Status::FAILURE;
+        throw std::runtime_error("FallbackNode::tick() - No children to tick");
     }
-    // TODO
-
-    // DEBUG
-    return Status::SUCCESS;
+    
+    for (auto& child : children_) {
+        status_ = child->tick();
+        if (status_ == Status::FAILURE) {
+            // move on to the next child
+            if (*child == *(children_.back())) {
+                // if this is the last child, return failure
+                std::cerr << "FallbackNode::tick() WARNING - Try to run the next child after a failed one but found no subsequent node. Returning a FAILURE status" << std::endl;
+                return status_;
+            }
+            else {
+                continue;
+            }
+        }
+        else {
+            // either the child returns running or success
+            return status_;
+        }
+    }
+    status_ = Status::FAILURE;
+    return status_;
 }
 
 FallbackNode::Ptr
